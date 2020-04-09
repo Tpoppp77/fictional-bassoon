3035695bc28ad5b86fd2888284ed00411e3c9c5b
Tpoppp <olson0002@yahoo.com>
2020-04-08 19:05:11 -0700

    parameters set in place to capture user input

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ function buildGoogleMapsQueryURL () {
     let placesURL = "https://maps.googleapis.com/maps/api/place/details/json?"
 
     let placesParams = { "api-key": "AIzaSyB4cz-a_rR5ipY6dvofPLFZlXM8rqBf9m4"};
+//capture user input
+    placesParams.q = $("")
+    .val()
+    .trim();
     
 };
 //here-geocode api
@@ -11,10 +15,16 @@ function buildLandmarkQueryURL () {
     let landmarkURL = "https://geocoder.ls.hereapi.com/6.2/geocode.json?";
 
     let landmarkParams = { "api-key": "ycuPAj5Im1MQ8vetcyjwUHNdZIyMVSXumgNZexhZbVw"};
+    
 };
 //open weather api
 function buildWeatherQueryURL () {
     let weatherURL = "https://api.openweathermap.org/data/2.5/weather?";
 
     let weatherParams = { "api-key": "8687ae869660372bd7eda0e4209f6ab5"};
+    
+    weatherParams.q = $("")
+    .val()
+    .trim();
+    
 };
