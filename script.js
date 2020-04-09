c524b055eb810fcd06255895ac4fdbf23b1e317b
Tpoppp <olson0002@yahoo.com>
2020-04-08 17:20:24 -0700

    added the query urls and script tags for apis

---
+++ script.js
@@ -0,0 +1,12 @@
+function buildGoogleMapsQueryURL () {
+    let placesURL = "";
+    
+};
+
+function buildLandmarkQueryURL () {
+    let landmarkURL = "https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=ycuPAj5Im1MQ8vetcyjwUHNdZIyMVSXumgNZexhZbVw";
+};
+
+function buildWeatherQueryURL (){
+    let weatherURL = "api.openweathermap.org/data/2.5/weather?q=scottsdale,arizona&appid=8687ae869660372bd7eda0e4209f6ab5";
+}
