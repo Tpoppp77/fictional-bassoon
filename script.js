582a1de4c4ec5574dad6626954bcffd5c7ad2f6f
Tpoppp <olson0002@yahoo.com>
2020-04-08 18:43:46 -0700

    added parameters and comments

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+//building the urls
+//google maps-places api
 function buildGoogleMapsQueryURL () {
-    let placesURL = "";
+    let placesURL = "https://maps.googleapis.com/maps/api/place/details/json?"
+
+    let placesParams = { "api-key": "AIzaSyB4cz-a_rR5ipY6dvofPLFZlXM8rqBf9m4"};
     
 };
-
+//here-geocode api
 function buildLandmarkQueryURL () {
-    let landmarkURL = "https://geocoder.ls.hereapi.com/6.2/geocode.json?apiKey=ycuPAj5Im1MQ8vetcyjwUHNdZIyMVSXumgNZexhZbVw";
-};
+    let landmarkURL = "https://geocoder.ls.hereapi.com/6.2/geocode.json?";
 
-function buildWeatherQueryURL (){
-    let weatherURL = "api.openweathermap.org/data/2.5/weather?q=scottsdale,arizona&appid=8687ae869660372bd7eda0e4209f6ab5";
-}
+    let landmarkParams = { "api-key": "ycuPAj5Im1MQ8vetcyjwUHNdZIyMVSXumgNZexhZbVw"};
+};
+//open weather api
+function buildWeatherQueryURL () {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?";
+
+    let weatherParams = { "api-key": "8687ae869660372bd7eda0e4209f6ab5"};
+};
